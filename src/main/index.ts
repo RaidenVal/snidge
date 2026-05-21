98c87b6abf157f8d42113a76bfc0445822949e28
RaidenVal <jolenezou711@gmail.com>
2026-05-21 16:47:38 +0100

    tray menu + global hotkey

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,74 +1,45 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
-import { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+import { app, Menu, Tray, globalShortcut } from 'electron'
+import { electronApp } from '@electron-toolkit/utils'
+import trayIcon from '../../resources/tray-icon.png?asset'
 
-function createWindow(): void {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
-    show: false,
-    autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
-    webPreferences: {
-      preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
-    }
-  })
+let tray: Tray | null = null
 
-  mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
-
-  mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
-
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
-  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
-  } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
-  }
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('com.raidenval.snidge')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
+  // Set up tray icon and menu behaviour
+  tray = new Tray(trayIcon)
+  // Show some text when hover over the tray icon
+  tray.setToolTip('Snidge')
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Hotkey Set up',
+      click: () => {}
+    },
+    { type: 'separator' },
+    {
+      label: 'Quit',
+      click: () => app.quit()
+    }
+  ])
+  tray.setContextMenu(contextMenu)
+
+  // Set up global shortcut
+  const ret = globalShortcut.register('CommandOrControl+Shift+C', () => {
+    console.log('Snidge triggered')
   })
-
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'))
-
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (!ret) {
+    console.log('Hotkey Command / Control + Shift + C is already in use')
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('window-all-closed', () => {
+  // This can prevent the app from quiting when window close
+})
+
+// Clean the global shortcut when quiting the app
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll()
+})
